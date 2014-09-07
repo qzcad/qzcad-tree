ddec270fef4a7dc61399f8363373c844c4ce44f5
Sergey Choporoff <s.choporoff@gmail.com>
2014-09-07 12:02:01 +0300

    Виджет граничных условий: методы обмена информацией, загрузка и сохранение в xml

--- qzcad/boundaryconditionswidget.cpp
+++ qzcad/boundaryconditionswidget.cpp
@@ -1,8 +1,11 @@
 #include "boundaryconditionswidget.h"
 #include "ui_boundaryconditionswidget.h"
 #include <QMessageBox>
-#include "qtscriptfemcondition3d.h"
-#include "qtscriptforcecondition3d.h"
+#include <QFile>
+#include <QFileDialog>
+#include <QTextStream>
+#include <iostream>
+#include <QDomDocument>
 
 BoundaryConditionsWidget::BoundaryConditionsWidget(QWidget *parent) :
     QWidget(parent),
@@ -13,6 +16,7 @@ BoundaryConditionsWidget::BoundaryConditionsWidget(QWidget *parent) :
     ui->boundaryConditionsTable->setColumnWidth(2, 100);
     ui->boundaryConditionsTable->setColumnWidth(3, 100);
     isForceMode = false;
+    is3d = true;
 }
 
 BoundaryConditionsWidget::~BoundaryConditionsWidget()
@@ -25,30 +29,39 @@ int BoundaryConditionsWidget::conditionsCount()
     return ui->boundaryConditionsTable->rowCount();
 }
 
-FEMCondition3DPointer BoundaryConditionsWidget::conditionPointer(int i)
+QString BoundaryConditionsWidget::condition(int i)
 {
-    FEMCondition3DPointer condPointer = NULL;
-    if (isForceMode)
-    {
-        QString condition = ui->boundaryConditionsTable->item(i, 0)->text();
-        QString u = ui->boundaryConditionsTable->item(i, 1)->text();
-        QString v = ui->boundaryConditionsTable->item(i, 2)->text();
-        QString w = ui->boundaryConditionsTable->item(i, 3)->text();
-        condPointer = new QtScriptForceCondition3D(condition, u, v, w);
-    }
-    else
-    {
-        QString func = ui->boundaryConditionsTable->item(i, 0)->text();
-        bool isU = ui->boundaryConditionsTable->item(i, 1)->checkState() == Qt::CheckState::Checked;
-        double u = ui->boundaryConditionsTable->item(i, 1)->text().toDouble();
-        bool isV = ui->boundaryConditionsTable->item(i, 2)->checkState() == Qt::CheckState::Checked;
-        double v = ui->boundaryConditionsTable->item(i, 2)->text().toDouble();
-        bool isW = ui->boundaryConditionsTable->item(i, 3)->checkState() == Qt::CheckState::Checked;
-        double w = ui->boundaryConditionsTable->item(i, 3)->text().toDouble();
-        condPointer = new QtScriptFemCondition3D(func, isU, u, isV, v, isW, w);
-    }
+    return ui->boundaryConditionsTable->item(i, 0)->text();
+}
 
-    return condPointer;
+QString BoundaryConditionsWidget::u(int i)
+{
+    return ui->boundaryConditionsTable->item(i, 1)->text();
+}
+
+bool BoundaryConditionsWidget::isU(int i)
+{
+    return (ui->boundaryConditionsTable->item(i, 1)->checkState() == Qt::Checked);
+}
+
+QString BoundaryConditionsWidget::v(int i)
+{
+    return ui->boundaryConditionsTable->item(i, 2)->text();
+}
+
+bool BoundaryConditionsWidget::isV(int i)
+{
+    return (ui->boundaryConditionsTable->item(i, 2)->checkState() == Qt::Checked);
+}
+
+QString BoundaryConditionsWidget::w(int i)
+{
+    return ui->boundaryConditionsTable->item(i, 3)->text();
+}
+
+bool BoundaryConditionsWidget::isW(int i)
+{
+    return (ui->boundaryConditionsTable->item(i, 3)->checkState() == Qt::Checked);
 }
 
 void BoundaryConditionsWidget::setIsForceMode(bool mode)
@@ -81,6 +94,146 @@ void BoundaryConditionsWidget::on_delCondition_clicked()
         {
             QModelIndex index = indexes.at(i);
             ui->boundaryConditionsTable->removeRow(index.row());
+        }
+    }
+}
+bool BoundaryConditionsWidget::getIs3d() const
+{
+    return is3d;
+}
+
+void BoundaryConditionsWidget::setIs3d(bool value)
+{
+    is3d = value;
+    if (!is3d)
+    {
+        ui->boundaryConditionsTable->setColumnHidden(3, true);
+    }
+}
+
+
+void BoundaryConditionsWidget::on_saveConditions_clicked()
+{
+    QString fileName = QFileDialog::getSaveFileName(this, tr("Сохранить список условий"), "", tr("xml-файлы (*.xml)"));
+    if(!fileName.isEmpty())
+    {
+        QFile file(fileName);
+        if (!file.open(QFile::WriteOnly | QFile::Text))
+        {
+            std::cerr << "Ошибка: Невозможно открыть файл для записи " << qPrintable(fileName) << ": " << qPrintable(file.errorString()) << std::endl;
+            return;
+        }
+        QDomDocument doc;
+        QDomElement conditions = doc.createElement("conditions");
+        conditions.setAttribute("isForceMode", isForceMode);
+        for (int i = 0; i < ui->boundaryConditionsTable->rowCount(); i++)
+        {
+            QDomElement cond = doc.createElement("condition");
+            cond.setAttribute("isApplied", condition(i));
+            cond.setAttribute("u", u(i));
+            cond.setAttribute("v", v(i));
+            cond.setAttribute("w", w(i));
+            if (!isForceMode)
+            {
+                cond.setAttribute("isU", isU(i));
+                cond.setAttribute("isV", isV(i));
+                cond.setAttribute("isW", isW(i));
+            }
+            conditions.appendChild(cond);
+        }
+        doc.appendChild(conditions);
+        QTextStream out(&file);
+        out << doc.toString();
+        file.close();
+    }
+}
+
+void BoundaryConditionsWidget::on_loadConditions_clicked()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Открыть список условий"), "", tr("xml-файлы (*.xml);;Любой файл (*)"));
+    if(!fileName.isEmpty())
+    {
+        ui->boundaryConditionsTable->clear();
+        while (ui->boundaryConditionsTable->rowCount() > 0)
+        {
+            ui->boundaryConditionsTable->removeRow(0);
+        }
+
+        QFile file(fileName);
+        if (!file.open(QFile::ReadOnly | QFile::Text))
+        {
+            std::cerr << "Ошибка: Невозможно прочитать файл " << qPrintable(fileName) << ": " << qPrintable(file.errorString()) << std::endl;
+            return;
+        }
+        QString errorStr;
+        int errorLine;
+        int errorColumn;
+        QDomDocument doc;
+        if (!doc.setContent(&file, false, &errorStr, &errorLine, &errorColumn))
+        {
+            std::cerr << "Ошибка: При разборе файла в сточке " << errorLine << ", столбце " << errorColumn << ": " << qPrintable(errorStr) << std::endl;
+            return;
+        }
+        QDomElement root = doc.documentElement();
+        if (root.tagName() != "conditions")
+        {
+            std::cerr << "Ошибка: Файл не является списком условий" << std::endl;
+            return;
+        }
+        if (isForceMode != (root.attribute("isForceMode").toInt() == 1))
+        {
+            std::cerr << "Ошибка: ";
+            if (!isForceMode)
+            {
+                std::cerr << "Файл не является списком граничных условий";
+            }
+            else
+            {
+                std::cerr << "Файл не является списком нагрузок";
+            }
+            std::cerr << std::endl;
+            return;
+        }
+        QDomNode child = root.firstChild();
+        while (!child.isNull())
+        {
+            QDomElement cond = child.toElement();
+            if(cond.tagName() != "condition")
+            {
+                std::cerr << "Ошибка: Файл не является списком условий" << std::endl;
+                return;
+            }
+
+            ui->boundaryConditionsTable->insertRow(ui->boundaryConditionsTable->rowCount());
+
+            QTableWidgetItem *condItem = new QTableWidgetItem(cond.attribute("isApplied"));
+            ui->boundaryConditionsTable->setItem(ui->boundaryConditionsTable->rowCount() - 1, 0, condItem);
+
+            QTableWidgetItem *uItem = new QTableWidgetItem(cond.attribute("u"));
+            if (!isForceMode)
+            {
+                bool isU = (cond.attribute("isU").toInt() == 1);
+                uItem->setCheckState((isU == true) ? Qt::Checked : Qt::Unchecked);
+            }
+            ui->boundaryConditionsTable->setItem(ui->boundaryConditionsTable->rowCount() - 1, 1, uItem);
+
+            QTableWidgetItem *vItem = new QTableWidgetItem(cond.attribute("v"));
+            if (!isForceMode)
+            {
+                bool isV = (cond.attribute("isV").toInt() == 1);
+                vItem->setCheckState((isV == true) ? Qt::Checked : Qt::Unchecked);
+            }
+            ui->boundaryConditionsTable->setItem(ui->boundaryConditionsTable->rowCount() - 1, 2, vItem);
+
+            QTableWidgetItem *wItem = new QTableWidgetItem(cond.attribute("w"));
+            if (!isForceMode)
+            {
+                bool isW = (cond.attribute("isW").toInt() == 1);
+                wItem->setCheckState((isW == true) ? Qt::Checked : Qt::Unchecked);
+            }
+            ui->boundaryConditionsTable->setItem(ui->boundaryConditionsTable->rowCount() - 1, 3, wItem);
+
+            child = child.nextSibling();
         }
     }
 }
--- qzcad/boundaryconditionswidget.h
+++ qzcad/boundaryconditionswidget.h
@@ -2,12 +2,13 @@
 #define BOUNDARYCONDITIONSWIDGET_H
 
 #include <QWidget>
-#include "femcondition3d.h"
 
 namespace Ui {
 class BoundaryConditionsWidget;
 }
-
+/**
+ * @brief Виджет для ввода граничных условий и нагрузок
+ */
 class BoundaryConditionsWidget : public QWidget
 {
     Q_OBJECT
@@ -15,17 +16,70 @@ class BoundaryConditionsWidget : public QWidget
 public:
     explicit BoundaryConditionsWidget(QWidget *parent = 0);
     ~BoundaryConditionsWidget();
+    /**
+     * @brief Количество введенных условий
+     * @return
+     */
     int conditionsCount();
-    FEMCondition3DPointer conditionPointer(int i);
+    /**
+     * @brief Критерий отбора точек, попдающих под действие условия
+     * @param i Номер условия
+     * @return Текст с кодом функции критерия
+     */
+    QString condition(int i);
+    /**
+     * @brief Действие (в общем случае функция) в первом напралении
+     * @param i Номер условия
+     * @return Текст с кодом функции для действия в первом направлении
+     */
+    QString u(int i);
+    /**
+     * @brief Применять ли условие в первом направлении
+     * @param i Номер условия
+     * @return true, если условие применяется
+     */
+    bool isU(int i);
+    /**
+     * @brief Действие (в общем случае функция) во втором напралении
+     * @param i Номер условия
+     * @return Текст с кодом функции для действия во втором направлении
+     */
+    QString v(int i);
+    /**
+     * @brief Применять ли условие во втором направлении
+     * @param i Номер условия
+     * @return true, если условие применяется
+     */
+    bool isV(int i);
+    /**
+     * @brief Действие (в общем случае функция) в третьем напралении
+     * @param i Номер условия
+     * @return Текст с кодом функции для действия в третьем направлении
+     */
+    QString w(int i);
+    /**
+     * @brief Применять ли условие в третьем направлении
+     * @param i Номер условия
+     * @return true, если условие применяется
+     */
+    bool isW(int i);
     void setIsForceMode(bool mode);
+    bool getIs3d() const;
+    void setIs3d(bool value);
+
 private slots:
     void on_addCondition_clicked();
 
     void on_delCondition_clicked();
 
+    void on_saveConditions_clicked();
+
+    void on_loadConditions_clicked();
+
 private:
     Ui::BoundaryConditionsWidget *ui;
     bool isForceMode;
+    bool is3d;
 };
 
 #endif // BOUNDARYCONDITIONSWIDGET_H
