5f3dd9a24114fe32842a9496552ec8293009125c
Sergey Choporoff <s.choporoff@gmail.com>
2014-10-22 10:11:02 +0300

    Реализован метод переменной жесткости

--- qzfem/plasticfem.cpp
+++ qzfem/plasticfem.cpp
@@ -5,7 +5,7 @@ PlasticFem::PlasticFem(HexahedralMesh3D* mesh, const std::vector<double> &strain
 {
     const int stress_size = stress.size();
     std::vector<MechanicalParameters3D> layers;
-    int it = 1;
+    int it = 0; // начение множителя
     bool isRupture = false;
 
     for (int i = 0; i < stress_size; i++)
@@ -15,15 +15,44 @@ PlasticFem::PlasticFem(HexahedralMesh3D* mesh, const std::vector<double> &strain
     HexahedralFEM *hFem;
     std::cout << "Нулевое приближение..." << std::endl;
     hFem = new HexahedralFEM(mesh, layers[0], boundaryForces, boundaryConditions);
-    std::vector<double> sigma = hFem->sigma();
+    sigma_ = hFem->sigma();
+    sigmaX_ = hFem->sigmaX();
+    sigmaY_ = hFem->sigmaY();
+    sigmaZ_ = hFem->sigmaZ();
+    tauXY_ = hFem->tauXY();
+    tauYZ_ = hFem->tauYZ();
+    tauZX_ = hFem->tauZX();
+    u_ = hFem->u();
+    v_ = hFem->v();
+    w_ = hFem->w();
     delete hFem;
+    std::cout << "Нулевое приближение построено." << std::endl;
+    // @ цель этого участка кода "перепрыгнуть" зону упругости (линейный участок)
+    double sigmaMax = 0.0;
+    for (msh::UInteger i = 0; i < mesh->elementsCount(); i++)
+    {
+        if (sigmaMax < sigma_[i]) sigmaMax = sigma_[i];
+    }
+    it = (int)(stress[0] / sigmaMax); // во сколько раз начальная интенсивность меньше максимума для зоны упругости
+    std::cout << "Множитель линейного участка: " << it << std::endl;
+    vectorScale(sigma_, (double)it);
+    vectorScale(sigmaX_, (double)it);
+    vectorScale(sigmaY_, (double)it);
+    vectorScale(sigmaZ_, (double)it);
+    vectorScale(tauXY_, (double)it);
+    vectorScale(tauYZ_, (double)it);
+    vectorScale(tauZX_, (double)it);
+    vectorScale(u_, (double)it);
+    vectorScale(v_, (double)it);
+    vectorScale(w_, (double)it);
+    // @
     mesh->clearLayers();
     for (msh::UInteger i = 0; i < mesh->elementsCount(); i++) mesh->pushLayer(0);
     while (!isRupture)
     {
-        for (typename std::vector<double>::size_type i = 0; i < sigma.size(); i++)
+        for (typename std::vector<double>::size_type i = 0; i < sigma_.size(); i++)
         {
-            if (stress[stress_size - 1] <= sigma[i])
+            if (stress[stress_size - 1] <= sigma_[i])
             {
                 isRupture = true;
                 mesh->setLayer(i, mesh->layer(i) + 1);
@@ -32,21 +61,148 @@ PlasticFem::PlasticFem(HexahedralMesh3D* mesh, const std::vector<double> &strain
             {
                 for (int j = 0; j < stress_size - 1; j++)
                 {
-                    if (stress[j] < sigma[i])
+                    if (stress[j] < sigma_[i])
                     {
                         mesh->setLayer(i, j + 1);
                     }
                 }
             }
         }
-        hFem = new HexahedralFEM(mesh, layers, boundaryForces, boundaryConditions);
-        std::vector<double> sigma_new = hFem->sigma();
-        delete hFem;
-        for (typename std::vector<double>::size_type i = 0; i < sigma.size(); i++)
+        if (!isRupture)
         {
-            sigma[i] += sigma_new[i];
+            ++it;
+            hFem = new HexahedralFEM(mesh, layers, boundaryForces, boundaryConditions);
+            vectorSumAB(u_, hFem->u());
+            vectorSumAB(v_, hFem->v());
+            vectorSumAB(w_, hFem->w());
+            vectorSumAB(sigmaX_, hFem->sigmaX());
+            vectorSumAB(sigmaY_, hFem->sigmaY());
+            vectorSumAB(sigmaZ_, hFem->sigmaZ());
+            vectorSumAB(tauXY_, hFem->tauXY());
+            vectorSumAB(tauYZ_, hFem->tauYZ());
+            vectorSumAB(tauZX_, hFem->tauZX());
+            vectorSumAB(sigma_, hFem->sigma());
+            delete hFem;
+
+            std::cout << "Итерация " << it + 1 << " завершена. Множитель: " << it << std::endl;
         }
-        std::cout << "Итерация " << it << " завершена." << std::endl;
-        it++;
+    }
+    std::cout << "Конечно-элементный анализ завершен. Множитель: " << it << std::endl;
+}
+
+void PlasticFem::vectorSumAB(std::vector<double> &a, const std::vector<double> &b)
+{
+    for (typename std::vector<double>::size_type i = 0; i < a.size(); i++)
+    {
+        a[i] += b[i];
     }
 }
+
+void PlasticFem::vectorScale(std::vector<double> &a, double b)
+{
+    for (typename std::vector<double>::size_type i = 0; i < a.size(); i++)
+    {
+        a[i] *= b;
+    }
+}
+
+std::vector<double> PlasticFem::sigma() const
+{
+    return sigma_;
+}
+
+void PlasticFem::setSigma(const std::vector<double> &sigma)
+{
+    sigma_ = sigma;
+}
+
+std::vector<double> PlasticFem::tauZX() const
+{
+    return tauZX_;
+}
+
+void PlasticFem::setTauZX(const std::vector<double> &tauZX)
+{
+    tauZX_ = tauZX;
+}
+
+std::vector<double> PlasticFem::tauYZ() const
+{
+    return tauYZ_;
+}
+
+void PlasticFem::setTauYZ(const std::vector<double> &tauYZ)
+{
+    tauYZ_ = tauYZ;
+}
+
+std::vector<double> PlasticFem::tauXY() const
+{
+    return tauXY_;
+}
+
+void PlasticFem::setTauXY(const std::vector<double> &tauXY)
+{
+    tauXY_ = tauXY;
+}
+
+std::vector<double> PlasticFem::sigmaZ() const
+{
+    return sigmaZ_;
+}
+
+void PlasticFem::setSigmaZ(const std::vector<double> &sigmaZ)
+{
+    sigmaZ_ = sigmaZ;
+}
+
+std::vector<double> PlasticFem::sigmaY() const
+{
+    return sigmaY_;
+}
+
+void PlasticFem::setSigmaY(const std::vector<double> &sigmaY)
+{
+    sigmaY_ = sigmaY;
+}
+
+std::vector<double> PlasticFem::sigmaX() const
+{
+    return sigmaX_;
+}
+
+void PlasticFem::setSigmaX(const std::vector<double> &sigmaX)
+{
+    sigmaX_ = sigmaX;
+}
+
+std::vector<double> PlasticFem::w() const
+{
+    return w_;
+}
+
+void PlasticFem::setW(const std::vector<double> &w)
+{
+    w_ = w;
+}
+
+std::vector<double> PlasticFem::v() const
+{
+    return v_;
+}
+
+void PlasticFem::setV(const std::vector<double> &v)
+{
+    v_ = v;
+}
+
+std::vector<double> PlasticFem::u() const
+{
+    return u_;
+}
+
+void PlasticFem::setU(const std::vector<double> &u)
+{
+    u_ = u;
+}
+
--- qzfem/plasticfem.h
+++ qzfem/plasticfem.h
@@ -24,6 +24,62 @@ public:
      * Номера слоев будут соответствовать зонам пластичности
      */
     PlasticFem(HexahedralMesh3D* mesh, const std::vector<double> &strain, const std::vector<double> &stress, const double &nu, const std::vector<FEMCondition3DPointer> &boundaryForces, const std::vector<FEMCondition3DPointer> &boundaryConditions);
+    /// @brief getters and setters
+    /// @{
+    std::vector<double> u() const;
+    void setU(const std::vector<double> &u);
+
+    std::vector<double> v() const;
+    void setV(const std::vector<double> &v);
+
+    std::vector<double> w() const;
+    void setW(const std::vector<double> &w);
+
+    std::vector<double> sigmaX() const;
+    void setSigmaX(const std::vector<double> &sigmaX);
+
+    std::vector<double> sigmaY() const;
+    void setSigmaY(const std::vector<double> &sigmaY);
+
+    std::vector<double> sigmaZ() const;
+    void setSigmaZ(const std::vector<double> &sigmaZ);
+
+    std::vector<double> tauXY() const;
+    void setTauXY(const std::vector<double> &tauXY);
+
+    std::vector<double> tauYZ() const;
+    void setTauYZ(const std::vector<double> &tauYZ);
+
+    std::vector<double> tauZX() const;
+    void setTauZX(const std::vector<double> &tauZX);
+
+    std::vector<double> sigma() const;
+    void setSigma(const std::vector<double> &sigma);
+    /// @}
+protected:
+    /**
+     * @brief a = a + b
+     * @param a Выходной параметр (передача по ссылке)
+     * @param b Входной параметр
+     */
+    void vectorSumAB(std::vector<double> &a, const std::vector<double> &b);
+    /**
+     * @brief Увеличить значение каждого элемента вектора a в b раз
+     * @param a Вектор (входной-выходной параметр)
+     * @param b Множитель
+     */
+    void vectorScale(std::vector<double> &a, double b);
+private:
+    std::vector<double> u_; //!< Перемещения в первом направлении (x)
+    std::vector<double> v_; //!< Перемещения во втором направлении (y)
+    std::vector<double> w_; //!< Перемещения в третьем направлении (z)
+    std::vector<double> sigmaX_; //!< Нормальные компоненты напряжения, параллельные первому направлению (x)
+    std::vector<double> sigmaY_; //!< Нормальные компоненты напряжения, параллельные второму направлению (y)
+    std::vector<double> sigmaZ_; //!< Нормальные компоненты напряжения, параллельные третьему направлению (z)
+    std::vector<double> tauXY_; //!< Касательные компоненты напряжения, в плоскости 1-2 (x-y)
+    std::vector<double> tauYZ_; //!< Касательные компоненты напряжения, в плоскости 2-3 (y-z)
+    std::vector<double> tauZX_; //!< Касательные компоненты напряжения, в плоскости 3-1 (z-x)
+    std::vector<double> sigma_; //!< Интенсивность напряжений
 };
 
 #endif // PLASTICFEM_H
