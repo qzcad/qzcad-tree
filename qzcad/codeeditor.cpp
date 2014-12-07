2c0757b3bb6ec8a8dd349485180a87390807bf46
Sergey Choporoff <s.choporoff@gmail.com>
2014-12-07 13:03:09 +0200

    Авто отступ по табуляции

--- qzcad/codeeditor.cpp
+++ qzcad/codeeditor.cpp
@@ -7,6 +7,7 @@ CodeEditor::CodeEditor(QWidget *parent) :
     countCache_.first = -1;
     countCache_.second = -1;
     lineNumberArea_ = new LineNumberArea(this);
+    tabSymbols_ = 3; // ширина по умолчани - 3 символа
 
     connect(this, SIGNAL(blockCountChanged(int)), this, SLOT(updateLineNumberAreaWidth(int)));
     connect(this, SIGNAL(updateRequest(QRect,int)), this, SLOT(updateLineNumberArea(QRect,int)));
@@ -15,7 +16,8 @@ CodeEditor::CodeEditor(QWidget *parent) :
     updateLineNumberAreaWidth(0);
     highlightCurrentLine();
     setFont(QFont("Monospace"));
-    setTabStopWidth(fontMetrics().width(QLatin1Char(' ')) * 3); // ширина по умолчани - 3 символа
+    setTabStopWidth(fontMetrics().width(QLatin1Char(' ')) * tabSymbols_);
+    autoindent_ = true;
 }
 
 void CodeEditor::lineNumberAreaPaintEvent(QPaintEvent *event)
@@ -61,17 +63,40 @@ int CodeEditor::lineNumberAreaWidth()
     return space;
 }
 
-void CodeEditor::resizeEvent(QResizeEvent *event)
+void CodeEditor::resizeEvent(QResizeEvent *e)
 {
-    QPlainTextEdit::resizeEvent(event);
+    QPlainTextEdit::resizeEvent(e);
 
     QRect cr = contentsRect();
     lineNumberArea_->setGeometry(QRect(cr.left(), cr.top(), lineNumberAreaWidth(), cr.height()));
 }
 
+void CodeEditor::keyReleaseEvent(QKeyEvent *e)
+{
+    if (autoindent_ && (e->key() == Qt::Key_Return || e->key() == Qt::Key_Enter))
+    {
+        QStringList lines = toPlainText().split('\n');
+        int indexCurrentLine = textCursor().blockNumber();
+
+        if (indexCurrentLine == -1)
+            indexCurrentLine = lines.size()-1;
+
+        int numberOfTabulations = lines[indexCurrentLine-1].count('\t');
+
+        if(lines[indexCurrentLine-1].contains('{'))
+            numberOfTabulations ++;
+        else if(lines[indexCurrentLine-1].contains('}'))
+            numberOfTabulations--;
+
+        for (register int i = 0; i < numberOfTabulations; i++)
+            insertPlainText(QString('\t'));
+    }
+}
+
 void CodeEditor::updateLineNumberAreaWidth(int newBlockCount)
 {
     setViewportMargins(lineNumberAreaWidth(), 0, 0, 0);
+    Q_UNUSED(newBlockCount)
 }
 
 void CodeEditor::highlightCurrentLine()
@@ -113,3 +138,24 @@ void CodeEditor::setTabStopSymbols(int symbols)
 {
     setTabStopWidth(fontMetrics().width(QLatin1Char(' ')) * symbols);
 }
+bool CodeEditor::autoindent() const
+{
+    return autoindent_;
+}
+
+void CodeEditor::setAutoindent(bool autoindent)
+{
+    autoindent_ = autoindent;
+}
+
+int CodeEditor::tabSymbols() const
+{
+    return tabSymbols_;
+}
+
+void CodeEditor::setTabSymbols(int tabSymbols)
+{
+    tabSymbols_ = tabSymbols;
+    setTabStopWidth(fontMetrics().width(QLatin1Char(' ')) * tabSymbols_);
+}
+
--- qzcad/codeeditor.h
+++ qzcad/codeeditor.h
@@ -1,42 +1,10 @@
-/****************************************************************************
- **
- ** Copyright (C) 2014 Digia Plc and/or its subsidiary(-ies).
- ** Contact: http://www.qt-project.org/legal
- **
- ** This file is part of the examples of the Qt Toolkit.
- **
- ** $QT_BEGIN_LICENSE:BSD$
- ** You may use this file under the terms of the BSD license as follows:
- **
- ** "Redistribution and use in source and binary forms, with or without
- ** modification, are permitted provided that the following conditions are
- ** met:
- **   * Redistributions of source code must retain the above copyright
- **     notice, this list of conditions and the following disclaimer.
- **   * Redistributions in binary form must reproduce the above copyright
- **     notice, this list of conditions and the following disclaimer in
- **     the documentation and/or other materials provided with the
- **     distribution.
- **   * Neither the name of Digia Plc and its Subsidiary(-ies) nor the names
- **     of its contributors may be used to endorse or promote products derived
- **     from this software without specific prior written permission.
- **
- **
- ** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- ** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- ** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- ** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- ** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- ** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- ** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- ** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- ** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- ** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- ** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
- **
- ** $QT_END_LICENSE$
- **
- ****************************************************************************/
+/**
+  * @author Сергей Чопоров
+  * @date 7/12/2014
+  * @version 1.0.1
+  * @copyright Copyright 2014 Sergey Choporov. All rights reserved.
+  * This project is released under the GNU Lesser General Public License.
+  */
 #ifndef CODEEDITOR_H
 #define CODEEDITOR_H
 
@@ -49,11 +17,25 @@ class CodeEditor : public QPlainTextEdit
 {
     Q_OBJECT
 public:
+    /**
+     * @brief Конструктор
+     * @param parent Указатель на родительский виджет
+     */
     explicit CodeEditor(QWidget *parent = 0);
     void lineNumberAreaPaintEvent(QPaintEvent *event);
     int lineNumberAreaWidth();
+    int tabSymbols() const;
+
+    bool autoindent() const;
+
+
+public slots:
+    void setTabSymbols(int tabSymbols);
+    void setAutoindent(bool autoindent);
+
 protected:
-    void resizeEvent(QResizeEvent *event);
+    void resizeEvent(QResizeEvent *e);
+    void keyReleaseEvent(QKeyEvent *e);
 private slots:
     void updateLineNumberAreaWidth(int newBlockCount);
     void highlightCurrentLine();
@@ -62,6 +44,8 @@ private slots:
 private:
     QWidget *lineNumberArea_;
     QPair<int, int> countCache_;
+    bool autoindent_; //!< Включатель авто отступа
+    int tabSymbols_; //!< Количество символов - ширина табуляции
 };
 /**
  * @brief Класс площади для вывода номеров строк
