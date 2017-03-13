046a6e598d83f5f1f0a0c87ff1e87808cc3943ce
Sergey Choporoff <choporov@list.ru>
2017-03-13 22:53:31 +0200

    Tetrahedron

--- qzcad/tetrahedron.cpp
+++ qzcad/tetrahedron.cpp
@@ -1,6 +1,93 @@
 #include "tetrahedron.h"
 
-Tetrahedron::Tetrahedron()
+namespace msh {
+
+Tetrahedron::Tetrahedron(const UInteger &node_0, const UInteger &node_1, const UInteger &node_2, const UInteger &node_3)
 {
+    vertex_[0] = node_0;
+    vertex_[1] = node_1;
+    vertex_[2] = node_2;
+    vertex_[3] = node_3;
+}
+
+Tetrahedron::Tetrahedron(const Tetrahedron &tetrahedron)
+{
+    vertex_[0] = tetrahedron.vertex_[0];
+    vertex_[1] = tetrahedron.vertex_[1];
+    vertex_[2] = tetrahedron.vertex_[2];
+    vertex_[3] = tetrahedron.vertex_[3];
+}
+
+UInteger &Tetrahedron::operator [](int i)
+{
+    int d = i % 4;
+
+    if (i < 0)
+        return vertex_[(d == 0) ? d : d + 4];
+
+    return vertex_[d];
+}
+
+Tetrahedron &Tetrahedron::operator =(const Tetrahedron &tetrahedron)
+{
+    if (this != &tetrahedron)
+    {
+        vertex_[0] = tetrahedron.vertex_[0];
+        vertex_[1] = tetrahedron.vertex_[1];
+        vertex_[2] = tetrahedron.vertex_[2];
+        vertex_[3] = tetrahedron.vertex_[3];
+    }
+    return *this;
+}
+
+int Tetrahedron::verticesCount() const
+{
+    return 4;
+}
+
+UInteger Tetrahedron::vertexNode(int i) const
+{
+    int d = i % 4;
+
+    if (i < 0)
+        return vertex_[(d == 0) ? d : d + 4];
+
+    return vertex_[d];
+}
+
+int Tetrahedron::facesCount() const
+{
+    return 4;
+}
+
+UIntegerVector Tetrahedron::face(const int &i) const
+{
+    UIntegerVector face(3);
+    if (i == 0)
+    {
+        face[0] = vertex_[0];
+        face[1] = vertex_[2];
+        face[2] = vertex_[1];
+    }
+    else if (i == 1)
+    {
+        face[0] = vertex_[0];
+        face[1] = vertex_[1];
+        face[2] = vertex_[3];
+    }
+    else if (i == 2)
+    {
+        face[0] = vertex_[1];
+        face[1] = vertex_[2];
+        face[2] = vertex_[3];
+    }
+    else
+    {
+        face[0] = vertex_[2];
+        face[1] = vertex_[0];
+        face[2] = vertex_[3];
+    }
+    return face;
+}
 
 }
--- qzcad/tetrahedron.h
+++ qzcad/tetrahedron.h
@@ -1,11 +1,76 @@
 #ifndef TETRAHEDRON_H
 #define TETRAHEDRON_H
 
+#include "element.h"
 
+namespace msh {
+/**
+ * @brief Класс Tetrahedron - четырехугольный тетраэдр
+ * ^
+ * | Y
+ *    3____0
+ *    |%  /\
+ *    | %/  \
+ *    | / %  \
+ *    |/    % \
+ *  1 |_______%\ 2   ->x
+ *
+ *  /
+ * v Z
+ */
 class Tetrahedron : public Element
 {
 public:
-    Tetrahedron();
+    /**
+     * @brief Конструктор
+     * @param node_0 Номер нулевого узла
+     * @param node_1 Номер первого узла
+     * @param node_2 Номер второго узла
+     * @param node_3 Номер третьего узла
+     */
+    Tetrahedron(const UInteger &node_0, const UInteger &node_1, const UInteger &node_2, const UInteger &node_3);
+    /**
+     * @brief Конструктор копирования
+     * @param tetrahedron Объект тераэдр для копирования
+     */
+    Tetrahedron(const Tetrahedron &tetrahedron);
+    /**
+     * @brief Оператор доступа к вершине по индексу
+     * @param i Индекс вершины тетраэдра
+     * @return Номер узла, на который ссылается вершина
+     */
+    UInteger &operator [] (int i);
+    /**
+     * @brief Оператор присвоения
+     * @param hexahedral Объект-тетраэдр для копирования
+     * @return Копию объекта hexahedral
+     */
+    Tetrahedron &operator =(const Tetrahedron &tetrahedron);
+    /**
+     * @brief Количество узлов
+     * @return Количество узлов (вершин) в элементе (8)
+     */
+    virtual int verticesCount () const;
+    /**
+     * @brief Номер узла, на который указывает вершина элемента
+     * @param i Номер вершины
+     * @return Номер узла, соответствующего вершине с номером i
+     */
+    virtual UInteger vertexNode (int i) const;
+    /**
+     * @brief Количество граней, соответвующих элементу (необходимо для визуализации)
+     * @return Количество гранейn (6)
+     */
+    virtual int facesCount() const;
+    /**
+     * @brief face Грань элемента
+     * @param i Номер грани
+     * @return Грань элемента, номер которойс равен i
+     */
+    virtual UIntegerVector face(const int &i) const;
+private:
+    UInteger vertex_[4]; //!< Вершины тетраэдра
 };
+}
 
 #endif // TETRAHEDRON_H
